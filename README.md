e876d411c498273c35c223c584c60d6a7b62b29c
Lucy Kreipke <lkreipke@smith.edu>
2025-05-05 13:50:40 -0400

    push #1

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # CSC251_Final_Project
+
+TO DO:
+
+convert hostname to IP/continue if IP otherwise error
+
+send ping to target
+
+TCP scan: full connection, capture any initial info sent back
+
+SYN scan: send syn, end connection if host responds syn ack
+
+UDP scan: send UDP packet to each port, is ICMP error message- closed 
+
+scan in order/scan random 
+
+scan all/scan well known
+
+dots 
--- port_scanner.py
+++ port_scanner.py
@@ -1,1 +1,31 @@
-#hi there
+#! usr/bin/env python
+
+from concurrent.futures import ThreadPoolExecutor
+import socket
+from scapy.all import *
+
+def handle_hostname():
+    #resolve hostname to IP 
+
+    pass
+
+def connect_scan():
+    pass
+
+def syn_scan():
+    pass
+
+def udp_scan():
+    pass
+
+# def scan_all():
+#     pass
+
+
+
+def main():
+    pass 
+
+if __name__ == "__main__":
+    main()
+
